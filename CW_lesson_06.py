a3d7191bf8697763f715dd1d5712186c65ec85ce
Yehor Ornatskyi <e.ornatsky@gmail.com>
2025-10-14 12:03:57 +0300

    Add files via upload

---
+++ CW_lesson_06.py
@@ -0,0 +1,40 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+ret,frame1=cap.read()
+gray1=cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+gray1=cv2.convertScaleAbs(gray1,alpha=1.5,beta=5)
+
+
+while True:
+    ret,frame2=cap.read()
+    if not ret:
+        print("Віде скінчилось")
+        break
+    gray2=cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.convertScaleAbs(gray2, alpha=1.5, beta=5)
+    diff = cv2.absdiff(gray1,gray2)
+    _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt)>3000:
+            x,y,w,h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame2,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+    cv2.imshow('Video',frame2)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+
+
+
+
+cv2.release()
+cv2.destroyAllWindows()
+
+
